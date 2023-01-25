f51f82936f329450b71500c9a0a078bfadac4c78
jong-k <kjhan710@naver.com>
2023-01-25 18:40:20 +0900

    refactor: action 타입 대신 action creator를 전달하게 수정

--- src/features/song/songSlice.ts
+++ src/features/song/songSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { reset } from "../movie/movieSlice";
 
 const initialState: string[] = []; // 타입 에러를 방지하기 위해 밖에서 미리 정의
 
@@ -17,7 +18,9 @@ const songSlice = createSlice({
   },
   // extraReducers 를 통해 다른 slice의 action 발생을 감지하고 reducer 생성
   extraReducers(builder) {
-    builder.addCase("movie/reset", () => []);
+    // builder.addCase("movie/reset", () => []);
+    // 문자열을 직접 입력하지 않고 action creator만 전달해도 됨
+    builder.addCase(reset, () => []);
   },
 });
 
